767a58e5690c0f90302b90f89aac37a6c16fe68a
kusk <anderskusk@gmail.com>
2016-10-14 10:21:54 +0200

    Better error handling

--- proxybrute.py
+++ proxybrute.py
@@ -4,9 +4,14 @@ import sys
 import _thread
 
 if len(sys.argv) < 2:
-    print("ProxyBrute.py - Missing arguments - [URL]")
+    print("ProxyBrute.py - Missing arguments - [URL] - [Logfile] - [Linkfile]")
+    print("[URL]\t\t-\tOf target domain")
+    print("[Logfile]\t-\tVerbose log")
+    print("[Linkfile]\t-\tFinal links")
     sys.exit(0)
 
+
+# ConnectionResetError
 p1 = ProxyList()
 p1.load_file('xroxy.list')
 print("Starting 20 threads each with 5000 tries")
@@ -17,14 +22,18 @@ def get_site(threadName, none):
         proxies = { 'http': 'http://'+p1.random().address() }
         count = count-1
         try:
-            out = requests.get(sys.argv[1], proxies=proxies, timeout=3)
+            out = requests.get(sys.argv[1], proxies=proxies, timeout=7)
             #if out.url != sys.argv[1]:
-            print('Thread: '+threadName+'\t Proxy: '+str(proxies)+'\t URL: '+out.url+'['+str(out.status_code)+']')
+            print(threadName+','+str(proxies)+','+out.url+','+str(out.status_code))
         except requests.exceptions.ConnectTimeout:
             continue
         except requests.exceptions.ReadTimeout:
             continue
         except requests.exceptions.ConnectionError:
+            continue
+        except ConnectionResetError:
+            continue
+        except requests.packages.urllib3.exceptions.ProtocolError:
             continue
         except KeyboardInterrupt:
             print('Exiting')
@@ -46,11 +55,11 @@ try:
     _thread.start_new_thread(get_site, ("Thread-13", 13))
     _thread.start_new_thread(get_site, ("Thread-14", 14))
     _thread.start_new_thread(get_site, ("Thread-15", 15))
-    _thread.start_new_thread(get_site, ("Thread-10", 16))
-    _thread.start_new_thread(get_site, ("Thread-11", 17))
-    _thread.start_new_thread(get_site, ("Thread-12", 18))
-    _thread.start_new_thread(get_site, ("Thread-13", 19))
-    _thread.start_new_thread(get_site, ("Thread-14", 20))
+    _thread.start_new_thread(get_site, ("Thread-16", 16))
+    _thread.start_new_thread(get_site, ("Thread-17", 17))
+    _thread.start_new_thread(get_site, ("Thread-18", 18))
+    _thread.start_new_thread(get_site, ("Thread-19", 19))
+    _thread.start_new_thread(get_site, ("Thread-20", 20))
 except:
     print("Error: unable to start thread")
 
